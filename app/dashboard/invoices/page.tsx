63cbca723aa5d295d31e1e0cec96f2c30b7e3049
oscar_kng <itsupport1@auto-lubumbashi.com>
2024-05-16 09:53:37 +0200

    initial commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const invoices = () => {
+  return (
+    <div>Invoices Page</div>
+  )
+}
+export default invoices
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from './layouts'
+
+export default function page() {
+  return (
+    <Layout>
+      <div>Dashboard Page</div>
+    </Layout>
+  )
+
+
+
+}
